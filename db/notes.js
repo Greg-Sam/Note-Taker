039fb3bcd5b3b4787da2e84cd621c37f475fa8ac
Greg Samuelson <grssam@gmail.com>
2020-11-29 15:41:24 -0800

    notes card display progress

--- db/notes.js
+++ db/notes.js
@@ -1,3 +1,3 @@
-let notes = [{ "title": "First Note", "text": "This is a note about all of the things I have to do and don't want to forget." }]
+let notes = [{ "title": "First Note", "text": "This is a note about all of the things I have to do and don't want to forget." }, { "title": "Second Note", "text": "This is a note I created to check if a second note would be created by my foreach loop." }]
 
 module.exports = notes
