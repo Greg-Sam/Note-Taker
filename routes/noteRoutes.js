034ca97b701d43b1955bbec8a66bbbda87cf5464
Greg Samuelson <grssam@gmail.com>
2020-11-30 17:44:49 -0800

    frustration but small progress on delete

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -11,9 +11,22 @@ router.get('/notes', (req, res) => {
 })
 
 router.delete('/notes/:id', (req, res) => {
+  let id = req.params
   res.json(notes)
   console.log(notes)
+  let objectIndex = notes.findIndex(value => value.id === parseInt(id.id))
+  console.log(objectIndex)
+  notes.forEach((item, i) => {
+    if (i === objectIndex){
+      notes.splice(i, 1)
+    } else {
+      console.log('did not work')
+    }
+    
+  })
+  console.log(notes)
   
+
 })
 
 module.exports = router
